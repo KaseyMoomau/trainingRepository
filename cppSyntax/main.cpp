7d4e649d0d1f21dd92ff9bde73b406a95731b7c5
Kasey Moomau <kmoomau2@huskers.unl.edu>
2020-10-28 17:06:05 -0500

    finished fizzbuzz.cpp

--- cppSyntax/main.cpp
+++ cppSyntax/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main() 
 {
-    std::cout<< "Hello World!" <<std::endl; 
+ 
+	int num = 0;
+	long bigNum = 340000000000000;
+	float decimalNum = 34.000;
+	double bigDecimalNum = 3.14159123123123123123;
+	std::cout << "bigNum: "<< sizeof(bigNum) << "-bytes, value: " << bigNum << std::endl;
+	std::cout << "bigDecimalNum: " << sizeof(bigDecimalNum) << "-bytes, value: " << bigDecimalNum << std::endl; 
+	std::cout << "num: " << sizeof(num) << "-bytes, value: " << num << std::endl; 
+	std::cout << "decimalNum: " << sizeof(decimalNum) << "-bytes, value: "<< decimalNum << std::endl; 
+	
       
     return 0; 
 } 
